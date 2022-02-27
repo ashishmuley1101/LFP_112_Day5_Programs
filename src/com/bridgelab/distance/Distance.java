e8b9ffdf37e5e40a1ed0ee1e8856e3f566bcd527
ashishmuley1101 <ashishmuley1101@gmail.com>
2022-02-27 22:24:44 +0530

    Distance from origin prorgam done

---
+++ src/com/bridgelab/distance/Distance.java
@@ -0,0 +1,27 @@
+package com.bridgelab.distance;
+
+import java.util.Scanner;
+
+public class Distance {
+
+	public static void main(String[] args) {
+		
+			Scanner sc = new Scanner(System.in);
+			System.out.println("enter value for point x");
+			int x = sc.nextInt();
+			System.out.println("enter value for point y ");
+			int y = sc.nextInt();
+			double dist1 = distance(x, y);
+			
+			 System.out.println("Distance for the origin is  : " +dist1);
+			
+			sc.close();
+	}
+	public static double distance(int x, int y) {
+
+		double dist = Math.pow((x * x + y * y), 0.5);
+		return dist;
+
+	}
+
+}
